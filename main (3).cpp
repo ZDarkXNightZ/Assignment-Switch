e4a78efe47a3a2dc7efcc0ce144ae3f2768d8a69
Jeevan Singh <147223417+ZDarkXNightZ@users.noreply.github.com>
2023-11-02 13:43:35 -0400

    Add files via upload

---
+++ main (3).cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+int main()
+{
+int ch;
+
+    
+    cout<<"Which non-cable channel is your favorite (2, 4, 6, 8, 11)?\n";
+    cin>>ch;
+    
+    
+    if (ch==2)
+    {
+    cout<<"Channel 2 got top ratings last week!\n";
+    }
+    
+    else if (ch==4)
+    {
+    cout<<"Channel 4 shows primarily news!\n";
+    }
+    
+    else if (ch==6)
+    {
+    cout<<"Channel 6 shows old movies!\n";
+    }
+    
+    else if (ch==8)
+    {
+    cout<<"Channel 8 covers local events!\n";
+    }
+    
+    else if (ch==11)
+    {
+    cout<<"Channel 11 is public broadcasting!\n";
+    }
+    
+    else
+    {
+        cout<<"That channel does not exist. It must be a cable channel";
+    }
+    
+}
